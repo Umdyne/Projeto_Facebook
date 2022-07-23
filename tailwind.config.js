f4fe94da57d0dd692bac0a71c6d15feb79c0ecab
Umdyne <luansilva2357@gmail.com>
2022-07-22 22:49:05 -0300

    Falta_Centralizar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {    
+      colors: {
+      'azul_logo_fb': '#1877F2',
+      'azul_bg_fb': '#F0F2F5',
+      'verde_botao_fb': '#42B72A',
+      },
+      spacing: {
+        '580': '580p',
+        '380': '380p'
+      }
   },
   plugins: [],
+  }
 }
